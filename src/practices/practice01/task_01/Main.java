9dd427353804ef254588aa27a0927d928ecdb28f
SHISH82 <a.zhukoff28@mail.ru>
2025-12-14 19:55:24 +0300

    practice_01

---
+++ src/practices/practice01/task_01/Main.java
@@ -0,0 +1,37 @@
+package practices.practice01.task_01;
+
+class Птица {
+    public void летать() {
+        System.out.println("Птица летает");
+    }
+}
+
+class Воробей extends Птица {
+    @Override
+    public void летать() {
+        System.out.println("Воробей летает");
+    }
+}
+
+class Пингвин extends Птица {
+    @Override
+    public void летать() {
+        throw new UnsupportedOperationException("Пингвины не умеют летать");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Птица птица1 = new Воробей();
+        птица1.летать();
+
+        try {
+            Птица птица2 = new Пингвин();
+            птица2.летать();
+        } catch (UnsupportedOperationException e) {
+            System.out.println("Ошибка:" + e.getMessage());
+        }
+    }
+}
